45f9868b89ecfece0fcd3648477d116fa3662404
zsy <zhangshiyu01@do-global.com>
2021-02-24 18:00:42 +0800

    add model

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"gin-kit-demo/controllers"
 	"gin-kit-demo/conf"
+	"gin-kit-demo/g_rediscache"
+	"gin-kit-demo/model"
 	"gin-kit-demo/router"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -14,4 +17,14 @@ func main() {
 	viper.BindPFlags(pflag.CommandLine)
 	conf.InitConfig()
 	router.Include(controllers.Routers)
+	// 初始化路由
+	r := router.Init()
+	defer model.Db.Close()
+	// 初始化gorm
+	model.Init()
+	// 初始化redis
+	g_rediscache.Init()
+	if err := r.Run(":8080"); err != nil {
+		fmt.Printf("startup service failed, err:%v\n\n", err)
+	}
 }
